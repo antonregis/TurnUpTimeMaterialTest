e48d08b48a5ff0cbd6a5b7f04f43665690b0e405
antonregis <anton.regis@gmail.com>
2022-02-26 17:58:17 +1300

    Login test

---
+++ Login/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            // Launch TurnUp portal/website
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            // Identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+            // Identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // Click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            // Check if user is logged in successfully
+            IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+            if (hellohari.Text == "Hello hari!")
+            {
+                Console.WriteLine("Logged in successfully, test passed");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, test failed");
+            }
+
+            driver.Quit();
+        }
+    }
+}
