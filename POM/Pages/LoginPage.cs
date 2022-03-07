9fa6f482442d6711e9af79ad95ebd7c67450df18
antonregis <anton.regis@gmail.com>
2022-03-07 15:18:29 +1300

    Adding Page Object Model version of the project

---
+++ POM/Pages/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeMaterialTest.Pages
+{
+    internal class LoginPage
+    {
+        public void LoginSteps(IWebDriver driver) 
+        {
+
+
+            // Launch TurnUp portal/website
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            // Identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+            // Identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // Click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            // Check if user is logged in successfully
+            IWebElement hellohari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+        }
+    }
+}
---
+++ POM/Tests/TM_Tests.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace TimeMaterialTest.Pages
+{
+    internal class TM_Tests
+    {
+        static void Main(string[] args)
+        {
+            // Open chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            // Login to website
+            LoginPage loginPageObj = new LoginPage();
+            loginPageObj.LoginSteps(driver);
+
+            // Click from home page: Administration > Time & Materials
+            HomePage homePageObj = new HomePage();
+            homePageObj.GoToTMPage(driver);
+
+            // Create Time & Material (TM) record in TM page
+            TMPage tMPageObj = new TMPage();
+            tMPageObj.CreateTM(driver);
+
+            // Edit TM record
+            tMPageObj.EditTM(driver);
+
+            // Delete TM record
+            tMPageObj.DeleteTM(driver);
+        }
+    }
+}
